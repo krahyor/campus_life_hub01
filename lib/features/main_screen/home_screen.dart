c5a7f69199a760b1445af2bae8ed814f04c41d33
krahyor <gimgim611@gmail.com>
2025-07-23 10:36:30 +0700

    ✨ feat:add home page

---
+++ lib/features/main_screen/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../calendar_screen/calendar_screen.dart';
+import '../home_screen/home_screen.dart';
+import '../profile_screen/profile_screen.dart';
+import '../setting_screen/setting_screen.dart';
+
+class MainHomeScreen extends StatefulWidget {
+  @override
+  _MainHomeScreenState createState() => _MainHomeScreenState();
+}
+
+class _MainHomeScreenState extends State<MainHomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> widgetOptions = const [
+    HomeScreen(),
+    CalendarScreen(),
+    Center(child: Text('Add Habit', style: TextStyle(fontSize: 24))),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Noted')),
+      body: widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.blueGrey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add Habit'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
