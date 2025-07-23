3b7ee95d97e5440d3ac8868b74b6c5c877187a42
NaphatJM <NaphatJM.work@gmail.com>
2025-07-23 11:50:20 +0700

    feat: Enhance Profile Screen layout and styling; update MainHomeScreen app bar design

--- lib/ui/screens/profile_screen/profile_screen.dart
+++ lib/ui/screens/profile_screen/profile_screen.dart
@@ -5,8 +5,35 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile Screen', style: TextStyle(fontSize: 24)),
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/profile_picture.png'),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'User Name',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'user.email@example.com',
+            style: TextStyle(fontSize: 16, color: Colors.grey),
+          ),
+          const SizedBox(height: 16), // Added padding here
+          Container(
+            width: 350,
+            height: 400,
+            decoration: BoxDecoration(
+              color: Colors.grey[500],
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
